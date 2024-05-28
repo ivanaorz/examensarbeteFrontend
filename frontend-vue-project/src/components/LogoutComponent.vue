d1d50d3072a8eee70d50fe0e522f1f809b254173
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2024-05-28 11:00:05 +0200

    Setting up the project

---
+++ frontend-vue-project/src/components/LogoutComponent.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <button class="sign-out-button">Sign out</button>
+</div>
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+</style>
---
+++ frontend-vue-project/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LogoutComponentVue from '@/components/LogoutComponent.vue'
+</script>
+
+<template>
+  <main>
+    <LogoutComponentVue/>
+  </main>
+</template>
