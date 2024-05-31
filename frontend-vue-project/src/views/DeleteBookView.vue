f2f3fd42bd0dc536a32dc37ee8c07029bbdaa8ab
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2024-05-31 03:39:09 +0200

    Fixed bug in the delete book view

--- frontend-vue-project/src/views/DeleteBookView.vue
+++ frontend-vue-project/src/views/DeleteBookView.vue
@@ -1,48 +1,78 @@
+
 <template>
-    <div class="delete-book-view">
-      <LogoutButton class="logout-button" />
-      <h1>Delete book entry</h1>
-      <form @submit.prevent="deleteBook">
-        <label for="title">Title of the book you want to delete</label>
-        <input v-model="title" id="title" type="text" required />
-        <button type="submit">Delete</button>
-      </form>
-      <div v-if="message" class="message">{{ message }}</div>
-    </div>
-  </template>
+  <div class="delete-book-view">
+    <LogoutButton class="logout-button" />
+    <h1>Delete book entry</h1>
+    <div v-if="message" class="message">{{ message }}</div>
+    <div v-else-if="noBooksMessage" class="no-books-message">{{ noBooksMessage }}</div>
+    <form v-else @submit.prevent="deleteBook">
+      <label for="title">Title of the book you want to delete</label>
+      <select v-model="title" id="title" required>
+        <option v-for="book in books" :key="book.id" :value="book.title">{{ book.title }}</option>
+      </select>
+      <button type="submit">Delete</button>
+    </form>
+  </div>
+</template>
+
   
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import BookService from '../service/bookService';
-  import LogoutButton from '../components/LogoutButton.vue';
-  
-  export default defineComponent({
-    name: 'DeleteBookView',
-    components: {
-      LogoutButton
-    },
-    setup() {
-      const title = ref('');
-      const message = ref('');
-  
-      const deleteBook = async () => {
-        try {
-          const responseMessage = await BookService.deleteBook(title.value);
-          message.value = responseMessage;
-          title.value = '';  
-        } catch (error) {
-          message.value = (error as Error).message;
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+import BookService from '../service/bookService';
+import LogoutButton from '../components/LogoutButton.vue';
+import type { BookDetails } from '../model/bookDetails';
+
+export default defineComponent({
+  name: 'DeleteBookView',
+  components: {
+    LogoutButton
+  },
+  setup() {
+    const books = ref<BookDetails[]>([]);
+    const title = ref('');
+    const message = ref('');
+    const noBooksMessage = ref('');
+
+    const fetchBooks = async () => {
+      try {
+        const bookEntries = await BookService.getBooks();
+        if (!bookEntries || bookEntries.length === 0) {
+          message.value = 'You have no book entries yet.';
+          books.value = [];
+        } else {
+          books.value = bookEntries;
+          noBooksMessage.value = ''; 
         }
-      };
-  
-      return {
-        title,
-        message,
-        deleteBook
-      };
-    }
-  });
-  </script>
+      } catch (error) {
+        message.value = (error as Error).message;
+      }
+    };
+
+    const deleteBook = async () => {
+      try {
+        const responseMessage = await BookService.deleteBook(title.value);
+        message.value = responseMessage;
+        title.value = '';  
+      } catch (error) {
+        message.value = (error as Error).message;
+      }
+    };
+
+
+    onMounted(() => {
+      fetchBooks();
+    });
+
+    return {
+      books,
+      title,
+      message,
+      noBooksMessage,
+      deleteBook
+    };
+  }
+});
+</script>
   
   <style scoped>
   .delete-book-view {
